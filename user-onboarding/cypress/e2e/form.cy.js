91bce777a3a20498fa6091aa5aa2a45a3b4d4147
babotavo419 <one.faith419@gmail.com>
2023-06-01 06:58:16 -0400

    ok

--- user-onboarding/cypress/e2e/form.cy.js
+++ user-onboarding/cypress/e2e/form.cy.js
@@ -1,47 +1,52 @@
 describe('Form Submission', () => {
     beforeEach(() => {
-
-        cy.visit('http://192.168.1.216:3000');
+        
+        cy.visit(' http://localhost:3000');
     });
 
     it('fills the name input', () => {
         const inputName = 'Test Name';
-        cy.get('name')
+        cy.get('input[name="username"]')
             .type(inputName)
             .should('have.value', inputName);
     });
 
     it('fills the email input', () => {
         const inputEmail = 'test@test.com';
-        cy.get('email')
+        cy.get('input[name="email"]')
             .type(inputEmail)
             .should('have.value', inputEmail);
     });
 
     it('fills the password input', () => {
         const inputPassword = 'Test123!';
-        cy.get('password')
+        cy.get('input[name="password"]')
             .type(inputPassword)
             .should('have.value', inputPassword);
     });
 
     it('checks the terms of service box', () => {
-        cy.get('tos')
+        cy.get('input[name="tos"]')
             .check()
             .should('be.checked');
     });
 
     it('submits the form', () => {
-        cy.get('submit')
+        cy.get('input[type="submit"]')
             .click();
         
     });
 
     it('validates if an input is left empty', () => {
-        cy.get('submit')
+        
+        cy.get('input[name="username"]').clear();
+        cy.get('input[name="password"]').clear();
+        cy.get('input[name="email"]').clear();
+
+        cy.get('input[type="submit"]')
             .click();
 
-        cy.get('error')
+        cy.contains('Username is required')
             .should('be.visible');
     });
 });
